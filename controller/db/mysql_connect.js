3d0ad65bd7b0bc729632647dd691b62d84f53413
Alpha Diallo <boydiallo829@gmail.com>
2024-12-08 16:09:28 +0300

    update in eng

--- controller/db/mysql_connect.js
+++ controller/db/mysql_connect.js
@@ -1,16 +1,19 @@
-const mysql=require('mysql')
-require('dotenv/config')
-var dbConn=mysql.createConnection({
-    user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DB_NAME,
-    host:process.env.MYSQL_INSTANCE_NAME
-})
-dbConn.connect((err)=>{
-    if(!err){
-        console.log("Veritabanına Bağlantı Yapıldı")
-    }else{
-        console.log("Veritabanı Hatası"+err)
+const mysql = require('mysql');
+require('dotenv/config');
+
+var dbConn = mysql.createConnection({
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    host: process.env.MYSQL_INSTANCE_NAME
+});
+
+dbConn.connect((err) => {
+    if (!err) {
+        console.log("Connected to the Database");
+    } else {
+        console.log("Database Error: " + err);
     }
-})
-module.exports=dbConn
+});
+
+module.exports = dbConn;
